c86984e91c80503ee9af164c36635d5a37c76aba
Durga Prasad Mishra <durg0001@edu.zealand.dk>
2022-09-04 23:01:05 +0200

    Add project files.

---
+++ BadeTidsSystemet/Kreds.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadeTidsSystemet
+{
+    public class Kreds
+    {
+        public string Id { get; set; }
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public int AntelDeltagere { get; set; }
+
+        public Kreds (string Id, string Navn, string Adresse, int AntelDeltagere)
+        {
+            this.Id = Id;
+            this.Navn = Navn;
+            this.Adresse = Adresse;
+            this.AntelDeltagere= AntelDeltagere;
+        }
+        public override string ToString()
+        {
+            return $"ID:{Id}, Navn:{Navn}, Adresse:{Adresse}, Deltagere:{AntelDeltagere}";
+        }
+    }
+}
---
+++ BadeTidsSystemet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BadeTidsSystemet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("opgave 1");
+            Console.WriteLine(" Test Kreds Klassen");
+            Kreds k1 = new Kreds("Kreds", "Roskilde","maglegårdsvej 2", 10);
+
+            Kreds k2 = new Kreds("Kreds", "København", "Rådhuspladsen 2", 15);
+
+            Console.WriteLine(k1);
+            Console.WriteLine(k2);
+            Console.WriteLine();
+
+            Console.WriteLine("opgave 2");
+
+        }
+    }
+}
