4446a1b6675772a1019b85cf29f130fef2cc2ef6
Durga Prasad Mishra <durg0001@edu.zealand.dk>
2022-09-12 13:56:02 +0200

    ny

--- BadeTidsSystemet/Badetidsperiode.cs
+++ BadeTidsSystemet/Badetidsperiode.cs
@@ -6,7 +6,37 @@ namespace BadeTidsSystemet
 {
     public class Badetidsperiode
     {
+        Dictionary<int, Kreds> kredse;
 
-       
+       public  string Type { get; set; }  // Angiver typen af badeaktivitet – f.eks.
+        public DayOfWeek UgeDag { get; set; } //DayOfWeek Dag i ugen hvor man må bade.
+        public DateTime StartTidspunkt { get; set; }
+        public DateTime SlutTidspunkt { get; set; }
+
+        public Badetidsperiode()
+        {
+            kredse = new Dictionary<int, Kreds>();
+            kredse = new Dictionary<int, Kreds>();
+
+            Type = "Badning";
+            UgeDag = DayOfWeek.Sunday;
+            StartTidspunkt = new DateTime();
+            SlutTidspunkt = new DateTime();
+
+            Type = "Badning";
+            UgeDag = DayOfWeek.Monday;
+            StartTidspunkt = new DateTime();
+            SlutTidspunkt = new DateTime();
+        }
+
+        public override string ToString()
+        {
+           
+            return $"type :{ Type}, UgeDage : {UgeDag}, StartTidsPunkt : {StartTidspunkt.TimeOfDay}, SlutTidsPunkt : {SlutTidspunkt.TimeOfDay}"; 
+        }
+
+
+
     }
+    
 }
--- BadeTidsSystemet/Program.cs
+++ BadeTidsSystemet/Program.cs
@@ -18,6 +18,22 @@ namespace BadeTidsSystemet
 
             Console.WriteLine("opgave 2");
 
+            Badetidsperiode b1 = new Badetidsperiode();
+            b1.Type = "morgendukkert";
+            b1.UgeDag = DayOfWeek.Sunday;
+            b1.StartTidspunkt = new DateTime().AddHours(6).AddMinutes(30);
+            b1.SlutTidspunkt = new DateTime().AddHours(8).AddMinutes(15);
+
+            Badetidsperiode b2 = new Badetidsperiode();
+            b2.Type = "Aftensvaler";
+            b2.UgeDag = DayOfWeek.Monday;
+            b2.StartTidspunkt = new DateTime().AddHours(12).AddMinutes(30);
+            b2.SlutTidspunkt = new DateTime().AddHours(21).AddMinutes(15);
+
+            
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+
         }
     }
 }
